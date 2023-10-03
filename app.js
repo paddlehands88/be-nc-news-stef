2265db8321488b691f84672a8b559b932234a97c
admin <admin@admins-MacBook-Pro.local>
2023-10-03 16:31:37 +0100

    Qu 4 GET /api/articles/:article_id completed

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const { getTopics, getApiEndpoints } = require("./controllers/nc-news.controller");
+const { getTopics, getApiEndpoints, getArticleById } = require("./controllers/nc-news.controller");
 const express = require("express");
 const app = express();
 
@@ -8,17 +8,34 @@ app.get("/api", getApiEndpoints);
 
 app.get("/api/topics", getTopics);
 
+app.get("/api/articles/:article_id", getArticleById);
+
+
+
+// ERROR HANDLING MIDDLEWARE 
+
+app.use((err, req, res, next) => {
+    if (err.code === '22P02') {
+      res.status(400).send({ message: 'Invalid Input' });
+    } else next(err);
+  });
+
+app.use((err, req, res, next) => {
+    if (err.status && err.message) {
+        res.status(err.status).send({message: err.message});
+    } else next(err);
+});
+
+app.use((err, req, res, next) => {
+    console.log(err);
+    res.status(500).send({ message: 'Internal Server Error' });
+  });
 
 // PATH NOT FOUND
 
 app.all("/*", (req, res, next) => {
     res.status(404).send({ message: 'Path not found!' })
-})
-
-
-
-
-
+});
 
 
 module.exports = app;
