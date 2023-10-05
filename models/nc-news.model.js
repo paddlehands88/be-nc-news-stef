d192376ca39e2d6be43170db66004b567d0c9a17
admin <admin@admins-MacBook-Pro.local>
2023-10-05 12:19:30 +0100

    Qu5 GET/api/articles completed ready for PR

--- models/nc-news.model.js
+++ models/nc-news.model.js
@@ -12,6 +12,30 @@ exports.fetchTopics = () => {
         })
 };
 
+
+
+exports.fetchArticles = () => {
+    return db.query(
+        `SELECT 
+        articles.author,
+        articles.title,
+        articles.article_id,
+        articles.created_at,
+        articles.votes,
+        articles.article_img_url,
+        CAST(COUNT(comment_id) AS INT) AS comment_count
+        FROM articles
+        LEFT JOIN comments ON comments.article_id = articles.article_id
+        GROUP BY articles.article_id
+        ORDER BY created_at DESC;`)
+        .then(({rows}) => {
+            console.log(rows, "MODEL fetchArticles");
+            return rows;
+        })
+};
+
+
+
 exports.fetchArticleById = (article_id) => {
     return db.query(`SELECT * FROM articles WHERE article_id=$1;`, [article_id])
         .then(({rows}) => {
