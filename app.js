a57946a03655bb46b1dc5754d828614bc77baae0
admin <admin@admins-MacBook-Pro.local>
2023-10-06 15:34:30 +0100

    Qu 6 get/api/articles/:article_id/comments completed with test for valid article id with no comments posted yet

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
-const { getTopics, getArticles, getApiEndpoints, getArticleById, getCommentsByArticleId } = require("./controllers/nc-news.controller");
+const {
+  getTopics,
+  getArticles,
+  getApiEndpoints,
+  getArticleById,
+  getCommentsByArticleId,
+} = require("./controllers/nc-news.controller");
 const express = require("express");
 const app = express();
-
-
 
 app.get("/api", getApiEndpoints);
 
@@ -14,33 +18,29 @@ app.get("/api/articles/:article_id", getArticleById);
 
 app.get("/api/articles/:article_id/comments", getCommentsByArticleId);
 
-
-
-// ERROR HANDLING MIDDLEWARE 
-
-app.use((err, req, res, next) => {
-    if (err.code === '22P02') {
-      res.status(400).send({ message: 'Invalid Input' });
-    } else next(err);
-  });
-
-app.use((err, req, res, next) => {
-    if (err.status && err.message) {
-        console.log(err.status, err.message, "APP.JS");
-        res.status(err.status).send({message: err.message});
-    } else next(err);
-});
-
-app.use((err, req, res, next) => {
-    console.log(err);
-    res.status(500).send({ message: 'Internal Server Error' });
-  });
-
 // PATH NOT FOUND
 
 app.all("/*", (req, res, next) => {
-    res.status(404).send({ message: 'Path not found!' })
+  res.status(404).send({ message: "Not found!" });
 });
 
+// ERROR HANDLING MIDDLEWARE
+
+app.use((err, req, res, next) => {
+  if (err.code === "22P02") {
+    res.status(400).send({ message: "Invalid Input" });
+  } else next(err);
+});
+
+app.use((err, req, res, next) => {
+  if (err.status && err.message) {
+    res.status(err.status).send({ message: "Not found!" });
+  } else next(err);
+});
+
+app.use((err, req, res, next) => {
+  console.log(err);
+  res.status(500).send({ message: "Internal Server Error!" });
+});
 
 module.exports = app;
